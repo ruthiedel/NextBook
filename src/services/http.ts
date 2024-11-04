9065c1776963df84e7a695eaa502ac64c41f8f2e
ruthi <ruthiedel866@gmail.com>
2024-11-04 12:51:47 +0200

    seventh

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const http = axios.create({
-    baseURL: "http://localhost:3000/api/book",
+    baseURL: "api/book",
 
     headers: {
         "Content-type": "application/json"
